aef42909fca4b9884129d65e72869d149d6c5baf
salokin-rm <82483428+salokin-rm@users.noreply.github.com>
2023-06-26 20:48:18 -0300

    Add files via upload

---
+++ ex_4.py
@@ -0,0 +1,19 @@
+"""
+Programa: Nome do Usuário
+Descrição: Este programa lê armazena um nome e dá "olá".
+Autor: Nikolas Martins
+Data: 21/06/2023
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+# Entrada de dados
+
+nome = input(f"Qual o seu nome?: ") #variável transição
+
+# Processamento de dados
+    
+# Saída de dados
+
+print("Olá,",nome,"!") 
